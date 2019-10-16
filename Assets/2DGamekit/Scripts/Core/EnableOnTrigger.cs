7e55fc03a6cfe13e69fbcc88ff72d4971183eaf0
Ernesto Aranda <e.adv.98@gmail.com>
2019-10-16 17:15:01 +0300

    Enemies spawn
    Uploaded a world with spawns along with the spawn functionality

---
+++ Assets/2DGamekit/Scripts/Core/EnableOnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnTrigger : MonoBehaviour
+{
+    public GameObject Prefab;
+
+    private void OnTriggerEnter2D()
+    {
+        Prefab.SetActive(true);
+    }
+}

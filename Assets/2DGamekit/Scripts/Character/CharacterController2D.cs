b04a2a2e106838d7755da2d4c7ee0fd386c5beae
thöbe <mariesmo@orange.fr>
2019-10-30 12:03:50 +0200

    Remove useless stuff from character controller

--- Assets/2DGamekit/Scripts/Character/CharacterController2D.cs
+++ Assets/2DGamekit/Scripts/Character/CharacterController2D.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
     [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
     [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
 
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
     private bool m_Grounded;            // Whether or not the player is grounded.
     private Rigidbody2D m_Rigidbody2D;
+    private Transform m_Transform;
     private GameObject healthUI;
 
     private bool m_FacingRight = true;  // For determining which way the player is currently facing.
@@ -67,19 +67,13 @@ public class CharacterController2D : MonoBehaviour
     }
 
     public void Move(float move, bool jump) {
-
-        //only control the player if grounded or airControl is turned on
         if (m_Grounded)
         {
             if (jump) {
-                // Add a vertical force to the player.
                 m_Grounded = false;
                 m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
             } else {
-                // Move the character by finding the target velocity
-                Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-                // And then smoothing it out and applying it to the character
-                m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+                m_Rigidbody2D.velocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
 
                 if (move > 0 && !m_FacingRight) {
                     Flip();
@@ -123,8 +117,8 @@ public class CharacterController2D : MonoBehaviour
     //Displays some visual infos, for example a circle showing the range
     void OnDrawGizmos() {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(punchPos.position, punchRange);
-        Gizmos.DrawWireSphere(kickPos.position, kickRange);
+        //Gizmos.DrawWireSphere(punchPos.position, punchRange);
+        //Gizmos.DrawWireSphere(kickPos.position, kickRange);
     }
 
     public void Damage(float damage)
